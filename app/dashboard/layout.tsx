a0cd14801acd9cbfb3e233a78dbca32585037b5e
Aerley Lopez <Lopezkeller65@gmail.com>
2026-05-20 13:27:12 -0600

    mejora de color de barra en modo claro

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -28,7 +28,7 @@ export default function DashboardLayout({
         <AppSidebar />
 
         <main className="flex-1 w-full bg-gray-50 dark:dark:bg-dark-page">
-          <div className="p-4 z-50 flex-1 dark:bg-dark-page border-b w-full justify-between flex items-center gap-4 sticky top-0">
+          <div className="p-4 z-50 flex-1 bg-gray-50 dark:bg-dark-page border-b w-full justify-between flex items-center gap-4 sticky top-0">
             <div className="flex items-center gap-2">
               <SidebarTrigger />
             </div>
