cb32b5264f8f4ddb7698ad9b45b369c7d0fa2aa8
Aerley Lopez <Lopezkeller65@gmail.com>
2026-05-20 11:49:23 -0600

    dark mode

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -28,21 +28,19 @@ export default function DashboardLayout({
         <AppSidebar />
 
         <main className="flex-1 w-full bg-gray-50 dark:dark:bg-dark-page">
-          <div className="p-4 z-50 flex-1 border-b w-full justify-between flex items-center gap-4 sticky top-0">
+          <div className="p-4 z-50 flex-1 dark:bg-dark-page border-b w-full justify-between flex items-center gap-4 sticky top-0">
             <div className="flex items-center gap-2">
               <SidebarTrigger />
             </div>
-            <span className="font-medium text-black uppercase tracking-widest text-[14px]">
+            <span className="font-medium text-black dark:text-dark-text-primary uppercase tracking-widest text-[14px]">
               {productsInCart.length !== 0 ? selectedCustomer?.cardName : ""}
             </span>
             <CartISync />
           </div>
 
-          <div className="p-0">
-            <AuthGuard>
-              {children}
-            </AuthGuard>
-          </div>
+          <AuthGuard>
+            {children}
+          </AuthGuard>
         </main>
       </SidebarProvider>
     </ThemeProvider>
